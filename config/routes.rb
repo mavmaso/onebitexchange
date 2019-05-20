05770337011564a5a8a98817927fb0d57ac79bcd
Marcos Soares <mavmaso@gmail.com>
2019-05-20 17:47:18 -0300

    Add exchange resquest with test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :exchanges, only: [:index]
+  get 'exchanges/convert'
+  get '/', to: 'exchanges#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
