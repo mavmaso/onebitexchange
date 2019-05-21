0129dfd70192a8f66a2ef0642eb463395cab8851
Marcos Soares <mavmaso@gmail.com>
2019-05-21 16:17:47 -0300

    Exchange controller with test

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -3,5 +3,8 @@ class ExchangesController < ApplicationController
   end
 
   def convert
+    # value = params
+    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
+    render json: {"value": value}, status: 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :exchanges, only: [:index]
-  get 'exchanges/convert'
-  get '/', to: 'exchanges#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :exchanges, only: [:index]
+  root 'exchanges#index'
+  get 'convert', to: 'exchanges#convert'
 end
